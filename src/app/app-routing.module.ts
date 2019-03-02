22dec2447661356c30fb88039efeaffb66149ea5
parnianmhr <parnian@gamil.com>
2019-03-02 15:00:14 +0100

    navbar links fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProductsComponent } from './components/products/products.component';
+import { CheckOutComponent } from './components/check-out/check-out.component';
+import { MyOrdersComponent } from './components/my-orders/my-orders.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 import { OrderSuccessComponent } from './components/order-success/order-success.component';
-import { CheckOutComponent } from './components/check-out/check-out.component';
 
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'checkout', component: CheckOutComponent},
   { path: 'products', component: ProductsComponent },
-  { path: 'shopping-cart', component: ShoppingCartComponent },
+  { path: 'my/orders', component: MyOrdersComponent},
   { path: 'order-success', component: OrderSuccessComponent},
-  { path: 'checkout', component: CheckOutComponent}
+  { path: 'shopping-cart', component: ShoppingCartComponent }
 ];
 
 @NgModule({
