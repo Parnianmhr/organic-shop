c4e565009850a7c941e416d42d348c37f14ccb5a
parnianmhr <parnian@gamil.com>
2019-03-09 16:48:59 +0100

    table, paginator, sort material

--- src/app/components/admin-products/admin-products.component.ts
+++ src/app/components/admin-products/admin-products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, AfterViewInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { Subscription } from 'rxjs';
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
@@ -8,17 +8,30 @@ import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.scss']
 })
-export class AdminProductsComponent implements OnInit, OnDestroy {
+export class AdminProductsComponent implements OnInit, OnDestroy, AfterViewInit {
   products: any[];
   subscription: Subscription;
   query: string;
 
-  constructor(private productService: ProductService) {}
+  displayedColumns = ['id', 'title', 'price', 'category', 'edit'];
+  dataSource: MatTableDataSource<any>;
 
-  ngOnInit() {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(private productService: ProductService) {
     this.subscription = this.productService.getAll().subscribe(p => {
       this.products = p;
     });
+  }
+
+  ngOnInit() {
+    this.dataSource = new MatTableDataSource(this.products);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
   ngOnDestroy() {
