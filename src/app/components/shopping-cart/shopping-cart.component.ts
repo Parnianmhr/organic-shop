7b51237dd1ce5e6e9b4a15fa3ff5647878df70e1
parnianmhr <parnian@gamil.com>
2019-03-15 16:57:55 +0100

    building the shopping cart page

--- src/app/components/shopping-cart/shopping-cart.component.ts
+++ src/app/components/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
+import { Observable } from 'rxjs';
+import { ShoppingCart } from 'src/app/models/shopping-cart';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -6,10 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-cart.component.scss']
 })
 export class ShoppingCartComponent implements OnInit {
+  cart$;
 
-  constructor() { }
+  constructor(private shoppingCartService: ShoppingCartService) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.cart$ = await this.shoppingCartService.getCart();
+
   }
-
 }
--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -1,7 +1,14 @@
 import { ShoppingCartItem } from './shopping-cart-item';
 
 export class ShoppingCart {
-    constructor(public items: ShoppingCartItem[]) {}
+    items: ShoppingCartItem[] = [];
+
+    constructor(public itemsMap: { [productId: string]: ShoppingCartItem }) {
+        // tslint:disable-next-line: forin
+        for (const productId in itemsMap) {
+            this.items.push(itemsMap[productId]);
+        }
+    }
 
     get totalItemsCount() {
         let count = 0;
